875df0a3464d00b792fbf6cf64bd9e4b3a4efa92
pellbo123 <kkahfi199@gmail.com>
2024-05-03 14:54:44 +0900

    feat: signup

---
+++ codespace/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+app.users - {}
+app.posts - []
+app.idCnt = 1
+
+@app.route('/sign-up', methods=['POST'])
+def signUp():
+    newUser = request.json
+    newUser['id'] = app.idCnt
+    app.users[app.idCnt] = newUser
+    app.idCnt += 1
